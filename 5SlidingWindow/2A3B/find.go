18e5a92baaa3ec838a7d63bcb4936f3dd68d789e
EugeneAng9603 <80694609+EugeneAng9603@users.noreply.github.com>
2025-04-16 11:05:07 +0800

    Update find.go

--- 5SlidingWindow/2A3B/find.go
+++ 5SlidingWindow/2A3B/find.go
@@ -5,53 +5,93 @@ package main
 import (
 	"fmt"
 )
+// better way
+func longestSubstringWith2A3B(s string) string {
+	left := 0
+	countA, countB := 0, 0
+	maxLen := 0
+	start := 0
 
-func twoAthreeB(s string) (int, int) {
-	head := 0
-	result := 0
-	table := map[byte]int{}
-	start, end := 0, 0
-	//counter := 0
-	for end < len(s) {
-		if _, ok := table[s[end]]; !ok {
-			table[s[end]] = 0
-		}
-		table[s[end]] += 1
-
-		// lesser than 2A3B, more than 2A or more than 3B. Then update max. and Move left to correct position using
-		if table['A'] == 2 && table['B'] == 3 {
-			head = start
-			result = max(result, end-start+1)
-			end += 1
-			continue
+	for right := 0; right < len(s); right++ {
+		switch s[right] {
+		case 'A':
+			countA++
+		case 'B':
+			countB++
 		}
 
-		if table['B'] > 3 {
-			for s[start] != 'B' {
-				table[s[start]] -= 1
-				start += 1
+		// Shrink window if we have too many A's or B's
+		for countA > 2 || countB > 3 {
+			switch s[left] {
+			case 'A':
+				countA--
+			case 'B':
+				countB--
 			}
-			table[s[start]] -= 1
-			start += 1
-			end += 1
-			continue
-		} else if table['A'] > 2 {
-			for s[start] != 'A' {
-				table[s[start]] -= 1
-				start += 1
-			}
-			table[s[start]] -= 1
-			start += 1
-			end += 1
-			continue
+			left++
 		}
 
-		end += 1
-		fmt.Print(s[head : head+result])
-		fmt.Print("\n")
+		// Check for exactly 2 A's and 3 B's
+		if countA == 2 && countB == 3 {
+			if right-left+1 > maxLen {
+				maxLen = right - left + 1
+				start = left
+			}
+		}
 	}
-	return result, head
+
+	if maxLen == 0 {
+		return ""
+	}
+	return s[start : start+maxLen]
 }
+
+// func twoAthreeB(s string) (int, int) {
+// 	head := 0
+// 	result := 0
+// 	table := map[byte]int{}
+// 	start, end := 0, 0
+// 	//counter := 0
+// 	for end < len(s) {
+// 		if _, ok := table[s[end]]; !ok {
+// 			table[s[end]] = 0
+// 		}
+// 		table[s[end]] += 1
+
+// 		// lesser than 2A3B, more than 2A or more than 3B. Then update max. and Move left to correct position using
+// 		if table['A'] == 2 && table['B'] == 3 {
+// 			head = start
+// 			result = max(result, end-start+1)
+// 			end += 1
+// 			continue
+// 		}
+
+// 		if table['B'] > 3 {
+// 			for s[start] != 'B' {
+// 				table[s[start]] -= 1
+// 				start += 1
+// 			}
+// 			table[s[start]] -= 1
+// 			start += 1
+// 			end += 1
+// 			continue
+// 		} else if table['A'] > 2 {
+// 			for s[start] != 'A' {
+// 				table[s[start]] -= 1
+// 				start += 1
+// 			}
+// 			table[s[start]] -= 1
+// 			start += 1
+// 			end += 1
+// 			continue
+// 		}
+
+// 		end += 1
+// 		fmt.Print(s[head : head+result])
+// 		fmt.Print("\n")
+// 	}
+// 	return result, head
+// }
 
 func max(a, b int) int {
 	if a > b {
